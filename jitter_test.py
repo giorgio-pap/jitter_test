9058de132ed64d9916cbf21d1a56d62aa6a96ad8
giorgio-p <info.giorgiopapitto@gmail.com>
2020-03-23 15:18:18 +0100

    Update jitter_test.py

--- jitter_test.py
+++ jitter_test.py
@@ -1,22 +1,25 @@
 good_sum = False
 
 while not good_sum:
-    x1=[2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6]
+    x1=[2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6] # possible duration of a jitter
     x1 = np.repeat(x1,26)
+   
+    # pick 228 values to match number of total trials (incl. fillers)
+    x_r_1 = random.sample(x1, 228) # fixation cross
+    x_r_2 = random.sample(x1, 228) # inter cue interval (ICI)
     
-    x_r_1 = random.sample(x1, 228)
-    x_r_2 = random.sample(x1, 228)
-    
-    df_1 = pd.DataFrame({'Freq_1': x_r_1})
-    df_2 = pd.DataFrame({'Freq_2': x_r_2})
+    # create dataframe out of the same
+    df_1 = pd.DataFrame({'Freq_1': x_r_1}) # fixation
+    df_2 = pd.DataFrame({'Freq_2': x_r_2}) # ICI
 
     
-    df_conc = pd.concat([df_1, df_2], axis=1)
-    sum_1 = df_conc["Freq_1"].sum()
-    sum_2 = df_conc["Freq_2"].sum()
+    df_conc = pd.concat([df_1, df_2], axis=1) #make a new dataframe 
+    sum_1 = df_conc["Freq_1"].sum() #sum of fix jitters
+    sum_2 = df_conc["Freq_2"].sum() #sum of ICI jitters
     
-    if sum_1 + sum_2 > 1811: # maximum time of 43,48 minutes (all incl.)
-        good_sum = False
+    #limit the maximum duration of the experimnt 
+    if sum_1 + sum_2 > 1811: # 1811 = maximum time of 43,48 minutes
+        good_sum = False 
     elif sum_1 + sum_2 <= 1811:
         good_sum = True
         jitters_fix = x_r_1
